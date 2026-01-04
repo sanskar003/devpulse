ac4829ae66400e4238da4809ab3ff8a7746bbd38
sanskar <sanskar.003.02@gmail.com>
2026-01-04 16:15:01 +0530

    final fix

--- frontend/src/apollo/client.ts
+++ frontend/src/apollo/client.ts
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 const link = new HttpLink({
   uri: import.meta.env.VITE_GRAPHQL_URI,
 });
+console.log(import.meta.env.VITE_GRAPHQL_URI)
 
 export const client = new ApolloClient({
   link,
